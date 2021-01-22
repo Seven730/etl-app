b47b90ee3d18f8c31eb74b450b09e7c6df7e393e
Seven730 <michalwww3@gmail.com>
2021-01-22 21:03:18 +0100

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,72 @@
 import React, { useState } from "react";
 import "./App.scss";
-import {
-  Jumbotron,
-  Container,
-  Button,
-  Table,
-  Pagination,
-} from "react-bootstrap";
-import Data from "./Data";
+import { Jumbotron, Container, Button } from "react-bootstrap";
 import axios from "axios";
 import { CSVLink } from "react-csv";
+import DataTable from "react-data-table-component";
 
 function App(): JSX.Element {
-  const [data, setData] = useState<any>(false);
+  const [data, setData] = useState<any>();
   const [loading, setLoading] = useState<boolean>(false);
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [rowsPerPage, setRowsPerPage] = useState<number>(10);
   const [isTransformed, setIsTransformed] = useState<boolean>(false);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [dataCSV, setDataCSV] = useState<any>([]);
 
-  let dataArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "K"];
-  let dataArrayCSV = dataArray;
-
-  const indexofLastItem = currentPage * rowsPerPage;
-  const indexofFirstItem = indexofLastItem - rowsPerPage;
-  let currentData = dataArray.slice(indexofFirstItem, indexofLastItem);
-
-  const handleETL = () => {
-    handleExtract();
-    handleTransform();
-    handleLoad();
-  };
+  const columns = [
+    {
+      name: "Code",
+      selector: "code",
+      sortable: true,
+    },
+    {
+      name: "Currency",
+      selector: "currency",
+      sortable: true,
+    },
+    {
+      name: "Exchange rate",
+      selector: "mid",
+      sortable: true,
+    },
+  ];
 
   const handleExtract = async (): Promise<any> => {
     setLoading(true);
+    setIsTransformed(false);
+    setIsLoaded(false);
     await axios
-      .get("http://api.nbp.pl/api/exchangerates/tables/B/")
+      .get("http://api.nbp.pl/api/exchangerates/tables/A/today/")
       .then((data) => {
         setData(data);
         setLoading(false);
-        console.log("Data in Extract: ", data)
       });
   };
 
-  const handleTransform =  (): void => {
-    if (data) {
+  const handleTransform = (): void => {
+    if (data && !isTransformed) {
       setIsTransformed(true);
-      setData(Array.from(data.data[0].rates))
-      currentData = dataArray.slice(indexofFirstItem, indexofLastItem)
-    }
-    else {
-      alert("Extract data first")
+      console.log(data);
+      setData(Array.from(data.data[0].rates));
+    } else {
+      alert("Extract data first");
     }
   };
 
-  const handleLoad = async (): Promise<any> => {
-    console.log("Data in Loading: ", data)
-    await axios.post("", { data });
+  const handleLoad = (): void => {
+    if (isTransformed && !isLoaded) {
+      setDataCSV(data);
+      setIsLoaded(true);
+    } else {
+      alert("Transform data first");
+    }
   };
 
-  const handleClearDB = async (): Promise<any> => {
+  const handleClearDB = (): void => {
     setData(undefined);
     setIsTransformed(false);
-    await axios.post("", { data });
+    setIsLoaded(false);
   };
 
-  let items: JSX.Element[] = [];
-  for (let i = 1; i <= 10; i++) {
-
-  }
-  for (
-    let number = 1;
-    number <= Math.ceil(dataArray.length / rowsPerPage);
-    number++
-  ) {
-    items.push(
-      <Pagination.Item
-        key={number}
-        active={number === currentPage}
-        onClick={(): void => setCurrentPage(number)}>
-        {number}
-      </Pagination.Item>
-    );
-  }
-// IT WAS ME ALL ALONG LUKE
   return (
     <div className="app">
       <div className="app__container">
@@ -93,54 +77,46 @@ function App(): JSX.Element {
           </Container>
         </Jumbotron>
         <Container className="app__buttons">
-          <Button variant="primary" onClick={handleETL}>
-            ETL
-          </Button>
           <Button variant="primary" onClick={handleExtract}>
-            Extract only
+            Extract
           </Button>
           <Button variant="primary" onClick={handleTransform}>
-            Transform only
+            Transform
           </Button>
           <Button variant="primary" onClick={handleLoad}>
-            Load only
+            Load
           </Button>
           <Button variant="primary" onClick={handleClearDB}>
-            Clear Database
+            Clear DB
           </Button>
           <Button variant="primary">
-            <CSVLink data={dataArrayCSV.toString()} className="app--CSV">
-              Export to .csv
+            <CSVLink
+              data={dataCSV && !dataCSV.config ? dataCSV : []}
+              className="app--CSV">
+              Export data to .csv
             </CSVLink>
           </Button>
         </Container>
         {data && isTransformed ? (
           <Container>
-            <Table striped bordered hover size="sm">
-              <thead>
-                <tr>
-                  <th>Code</th>
-                  <th>Currency</th>
-                  <th>Exchange rate</th>
-                  <th>Export (.csv)</th>
-                </tr>
-              </thead>
-              <Data data={data && isTransformed ? data : []} />
-            </Table>
-            <Pagination>{items}</Pagination>
+            <DataTable
+              title={`Exchange Rates - ${new Date()
+                .toJSON()
+                .slice(0, 10)
+                .replace(/-/g, "/")}`}
+              columns={columns}
+              data={data}
+              defaultSortFieldId="code"
+              pagination
+            />
           </Container>
         ) : data && !isTransformed ? (
-          <h2 className="data--info">Data needs to be transformed</h2>
+          <h2 className="data data__info">Data needs to be transformed</h2>
         ) : loading ? (
-          <h2 className="data--info">Loading...</h2>
+          <h2 className="data data__info">Loading...</h2>
         ) : (
-          <h2 className="data--info">No data</h2>
+          <h2 className="data data__info">No data</h2>
         )}
-
-        <footer className="data--footer">
-          Copyright © {new Date().getFullYear()} Joanna Wojtowicz, Michał
-          Wojcieszak, Jacek Wyroba
-        </footer>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
@@ -11,5 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// reportWebVitals(console.log);
